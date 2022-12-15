8f75a8ab41cdc29d27907603efdaef38f928991d
Admin <kavyak6999@gmail.com>
2022-12-15 11:06:02 +0530

    factorial

---
+++ js/12.js
@@ -0,0 +1,38 @@
+/*var a = parseInt(prompt("Enter the value"));
+for(let i = 0; i<=a ; i++)
+{
+  if(i%2!==0)
+  {
+    console.log("number"+" "+ i);
+  }
+}*/
+
+
+var a = parseInt(prompt("Enter the value"));
+var i = 1;
+var factorial = 1;
+do
+{
+	factorial = factorial * i;
+	i++;
+	console.log(factorial);	
+}
+while(i<=a)
+	
+	
+/*var a = parseInt(prompt("Enter the value"));	
+var factorial = 1;
+for(var i=1;i<=a;i++)
+	{
+		factorial *= i;
+	}	
+	console.log(factorial);*/
+	
+	
+	
+	
+	
+	
+	
+
+	
